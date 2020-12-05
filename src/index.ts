2897ef6163299d469dafb6f2c0e82f8206b97166
Bitrey <info@bitrey.it>
2020-12-05 09:53:00 +0100

    remove content security

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import helmet from "helmet";
-app.use(helmet());
+// app.use(helmet.contentSecurityPolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 
 import path from "path";
 app.use(express.static(path.join(__dirname, "/public")));
